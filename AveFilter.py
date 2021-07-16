7cf703971cbfcccf1a10ceb25c8d4eb0e5719902
str8ece <na0o6yu2ki2@gmail.com>
2021-07-16 14:20:08 +0900

    Add title in README.md

---
+++ AveFilter.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+# load image (grayscale)
+# 入力画像をグレースケールで読み込み
+color = cv2.imread("./figs/butterfly.png")
+
+# Spatial filtering
+# (OpenCVで実装)
+dst = cv2.blur(color, ksize=(3, 3))
+
+# output
+# 結果を出力
+cv2.imwrite("./output/output.png", dst)
